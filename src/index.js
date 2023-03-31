f1704c257a8b1cf7fdf847a4603c72c256aa4960
ikael28 <gaelkahuich@gmail.com>
2023-03-30 22:05:08 -0500

    Conexion con MongoAtlas Exitosa v1.3

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const config = require('../config');
 const indexRoutes = require('./routes/index');
 const authRoutes = require('./routes/auth');
+const path = require('path');
 
 const app = express();
 
@@ -28,14 +29,14 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', path.join(__dirname, 'views'));
 
 // Database connection
 mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(config.port))
   .catch(err => console.log(err));
 
-  app.listen(config.hostxd, () => {
+  app.listen(config.hostxd || config.port, () => {
     console.log('El servidor esta funcionando correctamente');
   });
 
